3ce22110e89720d6cbb48424c6866a34989dbcc8
JustPolar1 <elosopanda115@gmail.com>
2025-01-26 18:57:36 -0600

    Olvidé agregar esto lol

--- bd.sql
+++ bd.sql
@@ -6,3 +6,10 @@ CREATE TABLE maestros (
     maestro_estado BOOLEAN DEFAULT TRUE
 );
 
+CREATE TABLE especialidades (
+    especialidad_id INT PRIMARY KEY AUTO_INCREMENT,
+    especialidad_nombre VARCHAR(64) NOT NULL,
+    especialidad_estado BOOLEAN DEFAULT TRUE
+);
+
+ALTER TABLE maestros ADD COLUMN especialidad_id INT;
